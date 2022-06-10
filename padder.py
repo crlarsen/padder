451537ceae29e0813dd9792b6142eb667ee493a4
Chris Larsen <larsencr@gmail.com>
2022-06-09 20:28:28 -0700

    Move utility routines into a separate file.
    The modules Gij, PijGij, and Sum we being created for every adder.
    There are often multiple adders which become part of a circuit and
    the compiler to compiling these utility routines multiple times and
    warning of the duplicate modules.
    
    To clean things up the PijGij.v holds these 3 utility modules in a
    separate file.

--- padder.py
+++ padder.py
@@ -79,32 +79,6 @@ print(
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-// This module exists to keep the Verilog compiler from complaining about
-// outputs not being connected to inputs. Otherwise, I would have just used
-// the module PijGij everywhere and been done with it. Also, it does use a few
-// less gates than the PijGij module but I suspect a good Verilog compiler
-// would make such optimizations for unused outputs automatically.
-module Gij(\Pi:k , \Gi:k , \Gk-1:j , \Gi:j );
-  input \Pi:k , \Gi:k , \Gk-1:j ;
-  output \Gi:j ;
-
-  assign \Gi:j = \Gi:k | (\Pi:k  & \Gk-1:j );
-endmodule
-
-module PijGij(\Pi:k , \Pk-1:j , \Gi:k , \Gk-1:j , \Pi:j , \Gi:j );
-  input \Pi:k , \Pk-1:j , \Gi:k , \Gk-1:j ;
-  output \Pi:j , \Gi:j ;
-
-  assign \Pi:j = \Pi:k & \Pk-1:j ;
-  assign \Gi:j = \Gi:k | (\Pi:k  & \Gk-1:j );
-endmodule
-
-module Sum(\Gi-1:-1 , Ai, Bi, Si);
-  input \Gi-1:-1 , Ai, Bi;
-  output Si;
-
-  assign Si = \Gi-1:-1 ^ Ai ^ Bi;
-endmodule
 '''[1:] % (dt, count, count))
 
 print(
